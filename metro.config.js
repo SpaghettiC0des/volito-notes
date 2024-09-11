910fb3081c70dc0adf9a1850175d606613b5a61f
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-09-11 21:19:17 +0800

    chore: disable css output, not needed since we are not supporting web

--- metro.config.js
+++ metro.config.js
@@ -6,15 +6,16 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
+  isCSSEnabled: false,
 });
 
 // Enable Tamagui and add nice web support with optimizing compiler + CSS extraction
+// eslint-disable-next-line import/order
 const { withTamagui } = require("@tamagui/metro-plugin");
 module.exports = withTamagui(config, {
   components: ["tamagui"],
   config: "./tamagui.config.ts",
-  outputCSS: "./tamagui-web.css",
+  // outputCSS: "./tamagui-web.css",
 });
 
 config.resolver.sourceExts.push("mjs");
