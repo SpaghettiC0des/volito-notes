0d96ae898ff922be99e5e5d1b4bf8fc231e563fb
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-09-13 22:13:01 +0800

    fix: hide welcome nav header

--- features/welcome/WelcomeView.tsx
+++ features/welcome/WelcomeView.tsx
@@ -1,30 +1,34 @@
 import { NotepadText } from "@tamagui/lucide-icons";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { Fragment } from "react/jsx-runtime";
 import { Button, SizableText, View, YStack } from "tamagui";
 
 export function WelcomeView() {
   return (
-    <View flex={1} justifyContent="center">
-      <YStack alignSelf="center" width="70%">
-        <NotepadText alignSelf="center" color="$red9" size="$12" />
-        <SizableText
-          color="$gray10"
-          fontWeight="600"
-          mb="$4"
-          size="$9"
-          textAlign="center"
-        >
-          Volito Notes
-        </SizableText>
-        <Link asChild href="/(auth)/login-modal">
-          <Button bg="$red9" mb="$2">
-            Login
-          </Button>
-        </Link>
-        <Link asChild href="/(auth)/signup-modal">
-          <Button bg="$gray5">Sign up</Button>
-        </Link>
-      </YStack>
-    </View>
+    <Fragment>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View flex={1} justifyContent="center">
+        <YStack alignSelf="center" width="70%">
+          <NotepadText alignSelf="center" color="$red9" size="$12" />
+          <SizableText
+            color="$gray10"
+            fontWeight="600"
+            mb="$4"
+            size="$9"
+            textAlign="center"
+          >
+            Volito Notes
+          </SizableText>
+          <Link asChild href="/(auth)/login">
+            <Button bg="$red9" color="$white1" mb="$2">
+              Login
+            </Button>
+          </Link>
+          <Link asChild href="/(auth)/signup">
+            <Button variant="outlined">Sign up</Button>
+          </Link>
+        </YStack>
+      </View>
+    </Fragment>
   );
 }
