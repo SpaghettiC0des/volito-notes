a1d819b92cd234240d569db583e018fad9a0c531
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-09-15 23:36:29 +0800

    fix: add dialog provider

--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -1,6 +1,10 @@
 import { ToastProvider, ToastViewport } from "@tamagui/toast";
 import { useColorScheme } from "react-native";
-import { TamaguiProvider, type TamaguiProviderProps } from "tamagui";
+import {
+  PortalProvider,
+  TamaguiProvider,
+  type TamaguiProviderProps,
+} from "tamagui";
 
 import { CurrentToast } from "./CurrentToast";
 import { config } from "../tamagui.config";
@@ -18,20 +22,22 @@ export function Provider({
       defaultTheme={colorScheme === "dark" ? "dark" : "light"}
       {...rest}
     >
-      <ToastProvider
-        duration={6000}
-        native={
-          [
-            /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
-            // 'mobile'
-          ]
-        }
-        swipeDirection="horizontal"
-      >
-        <NavigationTheme>{children}</NavigationTheme>
-        <CurrentToast />
-        <ToastViewport left={0} right={0} top="$8" />
-      </ToastProvider>
+      <PortalProvider>
+        <ToastProvider
+          duration={6000}
+          native={
+            [
+              /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
+              // 'mobile'
+            ]
+          }
+          swipeDirection="horizontal"
+        >
+          <NavigationTheme>{children}</NavigationTheme>
+          <CurrentToast />
+          <ToastViewport left={0} right={0} top="$8" />
+        </ToastProvider>
+      </PortalProvider>
     </TamaguiProvider>
   );
 }
