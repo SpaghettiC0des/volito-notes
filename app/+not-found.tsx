ba9eaa361c4792f06bba202f37429c590a7c58e6
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-09-15 23:10:12 +0800

    chore: remove unused codes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -17,12 +17,6 @@ export default function NotFoundScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-    padding: 20,
-  },
   link: {
     marginTop: 15,
     paddingVertical: 15,
@@ -30,9 +24,5 @@ const styles = StyleSheet.create({
   linkText: {
     color: "#2e78b7",
     fontSize: 14,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
   },
 });
