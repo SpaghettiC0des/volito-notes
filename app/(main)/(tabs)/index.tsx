0762038bce3fb471089b057770749747bd186338
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-09-15 23:52:56 +0800

    feat: listen to snapshot changes

--- app/(main)/(tabs)/index.tsx
+++ app/(main)/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from "react";
+
 import {
   ChevronRight,
   Notebook,
@@ -6,8 +8,13 @@ import {
   MapPin,
 } from "@tamagui/lucide-icons";
 import { Link } from "expo-router";
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { FlatList } from "react-native";
 import { db } from "shared/firebase";
 import { Note } from "shared/models";
@@ -23,29 +30,71 @@ import {
 export default function NotesList() {
   const [notes, setNotes] = useState<Note[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const fn = async () => {
-      try {
-        setIsLoading(true);
-        const snapshot = await getDocs(collection(db, "notes"));
 
-        setNotes(
-          snapshot.docs.map(
-            (doc) =>
-              ({
-                id: doc.id,
-                ...doc.data(),
-              }) as Note,
-          ),
-        );
-      } catch (e) {
-        __DEV__ && console.error(e);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fn();
+  const fetchNotes = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const snapshot = await getDocs(collection(db, "notes"));
+
+      setNotes(
+        snapshot.docs.map(
+          (doc) =>
+            ({
+              id: doc.id,
+              ...doc.data(),
+            }) as Note,
+        ),
+      );
+    } catch (e) {
+      __DEV__ && console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
+
+  useEffect(() => {
+    const q = query(collection(db, "notes"), orderBy("date", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        console.log(change.type);
+        if (change.type === "added") {
+          setNotes((prev) => {
+            const next = [
+              ...prev,
+              { id: change.doc.id, ...change.doc.data() } as Note,
+            ];
+
+            return next
+              .sort((a, z) => {
+                const aDate = a.date.toMillis();
+                const zDate = z.date.toMillis();
+
+                return zDate - aDate;
+              })
+              .filter(
+                (v, i, a) =>
+                  a.findIndex((t) => t.id === v.id && t.name === v.name) === i,
+              );
+          });
+        }
+        if (change.type === "modified") {
+          setNotes((prev) =>
+            prev.map((note) =>
+              note.id === change.doc.id
+                ? ({ id: change.doc.id, ...change.doc.data() } as Note)
+                : note,
+            ),
+          );
+        }
+
+        if (change.type === "removed") {
+          setNotes((prev) => prev.filter((note) => note.id !== change.doc.id));
+        }
+      });
+      setIsLoading(false);
+    });
+    return unsubscribe;
+  }, [isLoading]);
 
   if (notes.length > 0) {
     return (
@@ -53,30 +102,39 @@ export default function NotesList() {
         <FlatList
           data={notes}
           ItemSeparatorComponent={Separator}
+          refreshing={isLoading}
           renderItem={({ item }) => (
-            <Link href={`/notes/${item.id}`} asChild>
+            <Link
+              asChild
+              href={{
+                pathname: `/notes/view`,
+                // @ts-expect-error
+                params: { ...item, date: item.date.toMillis() },
+              }}
+            >
               <ListItem
-                textProps={{ numberOfLines: 1 }}
                 hoverTheme
                 pressTheme
                 icon={item.location ? MapPin : Notebook}
                 iconAfter={ChevronRight}
                 subTitle={item.body}
+                textProps={{ numberOfLines: 1 }}
                 title={item.title}
               />
             </Link>
           )}
+          onRefresh={fetchNotes}
         />
         <Link asChild href="/notes/create">
           <Button
-            elevation="$2"
             circular
-            size="$6"
             bg="$red10"
+            bottom="$3"
+            elevation="$2"
             icon={<Pencil size="$2" />}
             pos="absolute"
             right="$2"
-            bottom="$3"
+            size="$6"
           />
         </Link>
       </YStack>
--- app/(main)/notes/view.tsx
+++ app/(main)/notes/view.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 import { LocationObject, getCurrentPositionAsync } from "expo-location";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { NoteForm, NoteFormSchema } from "features/notes";
 import { FirestoreError, Timestamp, doc, updateDoc } from "firebase/firestore";
 import { Fragment } from "react/jsx-runtime";
@@ -9,6 +9,7 @@ import { db } from "shared/firebase";
 import { NoteTransient } from "shared/models";
 
 export default function NoteById() {
+  const router = useRouter();
   const note = useLocalSearchParams() as any;
 
   const handleUpdate = useCallback(
@@ -22,12 +23,13 @@ export default function NoteById() {
           location = await getCurrentPositionAsync();
         }
 
-        return updateDoc(newNoteDocRef, {
+        await updateDoc(newNoteDocRef, {
           title,
           body,
           date: Timestamp.fromDate(date),
           location: location?.coords ?? null,
         } as NoteTransient);
+        router.back();
       } catch (e) {
         const error = e as FirestoreError;
         throw new Error(error.code);
